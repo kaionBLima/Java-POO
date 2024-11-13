04854ac65d3600bfdbe9a0a9804777fcb3a59fa2
kaionBLima <kaionbrandaolima@gmail.com>
2024-11-12 23:33:00 -0300

    Treinando questoes de arrays

---
+++ treinamentos/src/application/NegativoArrays.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class NegativoArrays {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos numeros voce vai digitar? ");
+        int n = sc.nextInt();
+        int[] vect = new int[n];
+
+        for (int i=0; i<vect.length; i++) {
+            System.out.printf("digite um numero: ");
+            vect[i] = sc.nextInt();
+        }
+
+        System.out.println("Números negativos:");
+
+        for (int i=0; i<vect.length; i++){
+            if (vect[i] < 0) {
+                System.out.printf("%d\n", vect[i]);
+            }
+        }
+
+        sc.close();
+
+    }
+
+}
---
+++ treinamentos/src/application/Soma_vetor.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Soma_vetor {
+
+    public static void main(String[] arg) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos numeros voce vai digitar?");
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+
+        for (int i=0; i<n; i++) {
+            System.out.printf("Digite um número: ");
+            vect[i] = sc.nextDouble();
+        }
+
+        System.out.printf("Valores: ");
+        for(int i=0; i<n; i++) {
+            System.out.printf("%.1f ", vect[i]);
+        }
+
+        double soma = 0.0;
+        for (int i = 0; i<n; i++) {
+            soma += vect[i];
+        }
+        System.out.printf("\nSoma: %.1f", soma);
+
+        double media = soma / n;
+        System.out.printf("\nMédia = %.1f%n", media);
+
+        sc.close();
+    }
+}
