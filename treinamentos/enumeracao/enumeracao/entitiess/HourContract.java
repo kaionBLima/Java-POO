28c7df56976c125bde55b770debdf5031782e887
kaionBLima <kaionbrandaolima@gmail.com>
2025-01-24 23:54:42 -0300

    Começando a trabalhar com composição em classes em diferentes pacotes

---
+++ treinamentos/enumeracao/enumeracao/entitiess/HourContract.java
@@ -0,0 +1,43 @@
+package entitiess;
+
+import java.util.Date;
+
+public class HourContract {
+
+    private Date date;
+    private Double valuePerHous;
+    private Integer hours;
+
+    public HourContract(){
+    }
+
+    public HourContract(Date date, Double valuePerHous, Integer hours) {
+        this.date = date;
+        this.valuePerHous = valuePerHous;
+        this.hours = hours;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getValuePerHous() {
+        return valuePerHous;
+    }
+
+    public void setValuePerHous(Double valuePerHous) {
+        this.valuePerHous = valuePerHous;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+}
