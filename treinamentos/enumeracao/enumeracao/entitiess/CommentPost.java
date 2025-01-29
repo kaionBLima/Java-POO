9285891d13b2ca022391cc92a839917f6ca3a882
kaionBLima <kaionbrandaolima@gmail.com>
2025-01-28 23:58:27 -0300

    Novo exercicio sobre enumeração e composição

---
+++ treinamentos/enumeracao/enumeracao/entitiess/CommentPost.java
@@ -0,0 +1,22 @@
+package entitiess;
+
+public class CommentPost {
+
+    private String text;
+
+    public CommentPost() {
+    }
+
+    public CommentPost(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
