a1dc5f02a45f465014c97b62851c9d6365e0fcbf
kaionBLima <kaionbrandaolima@gmail.com>
2025-01-31 23:59:12 -0300

    Começando exercício para treinar enumerção e composição

---
+++ treinamentos/enumeracao/enumeracao/entitiess/OrderItemBuy.java
@@ -0,0 +1,31 @@
+package entitiess;
+
+public class OrderItemBuy {
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItemBuy(){
+    }
+
+    public OrderItemBuy(Integer quantity, Double price) {
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
---
+++ treinamentos/enumeracao/enumeracao/entitiess/ProductBuy.java
@@ -0,0 +1,31 @@
+package entitiess;
+
+public class ProductBuy {
+
+    private String nome;
+    private Double price;
+
+    public ProductBuy() {
+    }
+
+    public ProductBuy(String nome, Double price) {
+        this.nome = nome;
+        this.price = price;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
