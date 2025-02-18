77c1a0cdcc197c552efd4c5fc295125d140ae6bc
kaionBLima <kaionbrandaolima@gmail.com>
2025-02-17 23:37:58 -0300

    Respondendo questao de Herança e polismorfismo

---
+++ treinamentos/Questoes_herancEpolimorf/src/programs/progProduct.java
@@ -0,0 +1,26 @@
+package programs;
+
+import entities.Product;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class progProduct {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/yyyy");
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        int n = sc.nextInt();
+        List <Product> listProduct = new ArrayList<>();
+
+        for()
+
+
+    }
+}
