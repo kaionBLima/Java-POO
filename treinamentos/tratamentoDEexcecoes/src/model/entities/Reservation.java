09c5193212b5c39ef65bcaf9d4d4a6fe0cb55481
kaionBLima <kaionbrandaolima@gmail.com>
2025-02-21 23:38:19 -0300

    Resolvendo questão de problema de execeção da pior forma Pt.1

---
+++ treinamentos/tratamentoDEexcecoes/src/model/entities/Reservation.java
@@ -0,0 +1,62 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private Date checkin;
+    private Date checkout;
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+        this.roomNumber = roomNumber;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckin() {
+        return checkin;
+    }
+
+    public Date getCheckout() {
+        return checkout;
+    }
+
+    public long duration() {
+        long diff = checkout.getTime() - checkin.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); //Tipo de conversor de tempo
+    }
+
+    public void uptadeDates(Date checkin, Date checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    @Override
+    public String toString () {
+        return "Room" +
+                roomNumber
+                + ", Check-in: "
+                + sdf.format(checkin)
+                + ", Check-out: "
+                + sdf.format(checkout)
+                + ", " + duration()
+                + " nights";
+    }
+
+}
