684e650ec1dcea48e6f4f2264d78c8011a0219ed
kaionBLima <kaionbrandaolima@gmail.com>
2025-03-17 22:44:52 -0300

    Excessão programa Reservation

--- treinamentos/tratamentoDEexcecoes/src/model/exception/DomainExpetion.java
+++ treinamentos/tratamentoDEexcecoes/src/model/exception/DomainExpetion.java
@@ -1,8 +1,8 @@
 package model.exception;
 
 //Classe que trata de exceção ou ás lança
-public class DomainExpetion extends RuntimeException{ /*Se for Exception, é obrigatória trata-la "throws" na hora de chama-la,
-                                              se for um RunTimeExpetion, basta chama-la que já funciona
+public class DomainExpetion extends RuntimeException{ /*Se for Exception, é obrigatória trata-la "throws" e propagar a exceção na hora de chama-la,
+                                              se for um RunTimeExpetion, não é obrigatório*/
 
     private static final long serialVersionUID = 1L; /*Classe Exception é uma classe serializable -> Uma classe serializável é uma
                                                        classe que pode ser transformada em um formato que possa ser armazenado ou transferido.*/
