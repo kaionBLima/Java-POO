f2956e9ea0874d010d9f53c7309ff4586771efa0
kaionBLima <kaionbrandaolima@gmail.com>
2025-03-21 23:15:03 -0300

    Mexendo com arquivos de forma menos rústica

--- treinamentos/trabComArquivos/src/application/FileBufferedReader.java
+++ treinamentos/trabComArquivos/src/application/FileBufferedReader.java
@@ -11,12 +11,8 @@ public class FileBufferedReader {
     public static void main(String[] args) throws FileNotFoundException {
 
         String path = "C:\\Users\\kaion\\arq1.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try{
-            fr = new FileReader(path); //Recebe e abre arquivo
-            br = new BufferedReader(fr);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine(); //Leitura de arquivo
 
@@ -28,18 +24,7 @@ public class FileBufferedReader {
         catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+
 
     }
 
